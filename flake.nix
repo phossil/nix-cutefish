08cbb71d6f7e319c99d532bdbd2014aa9a951b20
Phosu Parsons <38191182+phossil@users.noreply.github.com>
2023-03-26 17:49:50 -0400

    flake.nix: explicitly declare `nixpkgs`

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "A very basic flake";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ]
